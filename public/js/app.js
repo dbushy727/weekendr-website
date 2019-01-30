b4c0282236cbccc24516cce66b4b9e7011ce11fc
Danny Bushkanets <d.bushkanets@gmail.com>
2019-01-30 16:33:08 -0500

    remove template sass/js

--- public/js/app.js
+++ public/js/app.js
@@ -48113,8 +48113,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/DanielBushkanets/Coding/weekendr-website/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/DanielBushkanets/Coding/weekendr-website/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/bushkdan/sites/weekendr-website/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/bushkdan/sites/weekendr-website/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
